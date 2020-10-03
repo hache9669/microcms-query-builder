78fe00df95337fa5a2fa1dc620b679e8a94277f6
hache9669 <hm.ck.rc@gmail.com>
2020-10-04 05:29:55 +0900

    create index.js

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import builder from "./FilterBuilder";
+import query from "./MicroCMSQuery";
+import searchble from "./types/IMicroCMSSearchable";
+
+export const FilterBuilder = builder;
+export const MicroCMSQuery = query;
+export type MicroCMSSearchable = searchble;
